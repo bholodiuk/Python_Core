d54706a2b501c783333570f17da9bbf67d46f0bd
azul <azul@weareclario.com>
2020-12-04 21:39:14 +0200

    Home work 1 task2

---
+++ lesson1/hw1/hw1_task2.py
@@ -0,0 +1,18 @@
+"""
+Enter two values 'a' and 'b'
+and make calculations.
+a+b
+a-b
+a*b
+a/b
+a**b
+"""
+
+a = int(input('Input first value "a": '))
+b = int(input('Input first value "b": '))
+
+print(f"a + b = {a + b}\n"
+      f"a - b = {a - b}\n"
+      f"a * b = {a * b}\n"
+      f"a / b = {a / b}\n"
+      f"a**b = {a**b}")
