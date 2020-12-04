b5775a481b5538bd624e9f19adf3e48d28c4af4f
azul <azul@weareclario.com>
2020-12-04 21:30:25 +0200

    Home work 1 task1

---
+++ lesson1/hw1/hw1_task1.py
@@ -0,0 +1,7 @@
+user_name = input('Enter your name: ')
+user_age = input('Enter your age: ')
+user_city = input('Enter name of your city: ')
+
+print(f'\nYour name is {user_name}\n'
+      f'Your age is {user_age}\n'
+      f'You live in {user_city}')
