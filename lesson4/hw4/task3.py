c06b7035759408c37099ff5dc9d5cc10f9ac1337
Bohdan Holodiuk <azul@weareclario.com>
2020-12-08 11:50:52 +0200

    Home_Work4_Task_3 (Loops Slide 15)

---
+++ lesson4/hw4/task3.py
@@ -0,0 +1,17 @@
+"""
+Print Fibonacci numbers up to the entered number n,
+using cycles.
+(Sequence of Fibonacci numbers 0, 1, 1, 2, 3, 5, 8, 13, etc.)
+"""
+
+def fibonacci_sequence(n):
+    if n > 1:
+        return fibonacci_sequence(n-1) + fibonacci_sequence(n-2)
+    return n
+
+if __name__ == '__main__':
+    up_value = int(input('Enter fibonacci sequence max '))
+
+    for value in range(up_value):
+        if fibonacci_sequence(value) <= up_value:
+            print(fibonacci_sequence(value))
