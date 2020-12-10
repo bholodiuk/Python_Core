fae6f8ecc11108928c6286e4483f32e5233ed435
Bohdan Holodiuk <azul@weareclario.com>
2020-12-11 01:56:53 +0200

    Solution for Home_Work5_Task_2 ('Collection' Slide 62)

--- lesson6/hw6/task2.py
+++ lesson6/hw6/task2.py
@@ -4,3 +4,19 @@ If the login is "First", then greet the users. If the login is different, send a
 (need to use loop while)
 """
 
+def user_authentication():
+    user_login_correct = 'First'
+    detect_correct_answer = True
+
+    while detect_correct_answer:
+        user_login = input('Enter user login: ')
+
+        if user_login == user_login_correct:
+            print(f'Authentication is success, welcome user: {user_login}')
+            break
+        else:
+            print(f'User login -> {user_login} is incorrect')
+
+
+if __name__ == '__main__':
+    user_authentication()
