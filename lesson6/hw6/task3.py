a931b66eb8073c2bc49e1bbfb23e55dabf6b758f
Bohdan Holodiuk <azul@weareclario.com>
2020-12-11 04:45:42 +0200

    Solution of Home_Work6_Task_3 (Function Slide 39)

--- lesson6/hw6/task3.py
+++ lesson6/hw6/task3.py
@@ -1,3 +1,16 @@
 """
 Write a function that calculates the number of characters included in a given string
 """
+
+def string_len_calculation(input_string: str):
+    """
+    Calculating length of input string.
+    :param input_string: length calculated value.
+    :return: len(input_string) - length of input_string.
+    """
+    return len(input_string)
+
+if __name__ == '__main__':
+    value = input('Enter your string: ')
+
+    print(f'{value} consist of: {string_len_calculation(input_string=value)} characters.')
