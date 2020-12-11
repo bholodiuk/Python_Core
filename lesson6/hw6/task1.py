994be03167427096cc44310fb66dd38700446ad8
Bohdan Holodiuk <azul@weareclario.com>
2020-12-11 02:23:00 +0200

    Global changes in tasks

--- lesson6/hw6/task1.py
+++ lesson6/hw6/task1.py
@@ -1,19 +1,6 @@
 """
-In the range from 1 to 10 determine
-even numbers that are divisible by 2,
-odd numbers, which are divisible by 3,
-numbers that are not divisible by 2 and 3.
+Write a function that returns the largest number of two numbers
+(use DocStrings documentation strings in the function).
 """
 
-def numbers_determination():
-    for digit in range(1, 11):
-        if digit % 2 == 0:
-            print(f'{digit} is even')
-        elif digit % 3 == 0:
-            print(f'{digit} is odd')
-        else:
-            print(f'{digit} is not divisible by 2 and 3')
-
-
-if __name__ == '__main__':
-    numbers_determination()
+def max_value()
--- lesson7/hw7/task2.py
+++
@@ -1,4 +0,0 @@
-"""
-Write a program that calculates the square of ​​a rectangle, triangle and circle
-(write three functions to calculate the square, and call them in the main program depending on the user's choice).
-"""
