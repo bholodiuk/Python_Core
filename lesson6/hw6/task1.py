8612b66590f27c1b48d740897d21f3bf75b43267
Bohdan Holodiuk <azul@weareclario.com>
2020-12-11 03:52:34 +0200

    Solution for Home_Work6_Task_1 (Function Slide 31)

--- lesson6/hw6/task1.py
+++ lesson6/hw6/task1.py
@@ -3,4 +3,18 @@ Write a function that returns the largest number of two numbers
 (use DocStrings documentation strings in the function).
 """
 
-def max_value()
+def max_value(*args):
+    """
+    Method returns max value between 2 values.
+    :parameters:
+        *args: int, int - two integer values
+
+    :return:
+        max(args)
+    """
+    return max(args)
+
+if __name__ == '__main__':
+    value_one = int(input("Enter first value: "))
+    value_two = int(input("Enter second value: "))
+    print(f'Max value between {value_one} and {value_two} -> {max_value(value_one, value_two)}')
