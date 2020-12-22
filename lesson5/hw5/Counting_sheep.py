cd446d63941aaeb452a4de41c1b812b5e39a0499
Bohdan Holodiuk <azul@weareclario.com>
2020-12-22 12:51:47 +0200

    Solution for Home_Work5 Convert_boolean_values_to_strings_'Yes'_or_'No'.

---
+++ lesson5/hw5/Counting_sheep.py
@@ -0,0 +1,30 @@
+"""
+Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+For example,
+
+[True,  True,  True,  False,
+  True,  True,  True,  True ,
+  True,  False, True,  False,
+  True,  False, False, True ,
+  True,  True,  True,  True ,
+  False, False, True,  True]
+"""
+
+def count_sheeps(sheep):
+    """
+
+    :param sheep:
+    :return:
+    """
+    return sheep.count(True)
+
+if __name__ == '__main__':
+    array1 = [True, True, True, False,
+              True, True, True, True,
+              True, False, True, False,
+              True, False, False, True,
+              True, True, True, True,
+              False, False, True, True]
+
+    print(count_sheeps(array1), 17, "There are 17 sheeps in total, not %s" % count_sheeps(array1))
